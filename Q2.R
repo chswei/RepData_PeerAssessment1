e24bb2bc9a2cc81b14b6f2af94740db34ba3a07b
chswei <v09029w@gmail.com>
2017-09-10 14:44:56 +0800

    Revised loadproc.R & Finished Q2

---
+++ Q2.R
@@ -0,0 +1,21 @@
+# Loading and preprocessing the data
+if (!file.exists("./RepData_PeerAssessment1/figures")) {
+  dir.create("./RepData_PeerAssessment1/figures")
+}
+unzip("./RepData_PeerAssessment1/activity.zip", exdir = "./RepData_PeerAssessment1/activity")
+act <- read.csv("./RepData_PeerAssessment1/activity/activity.csv")
+act$date <- as.Date(act$date, "%Y-%m-%d")
+
+# Histogram of the total number of steps taken each day
+library(dplyr)
+library(ggplot2)
+sum <- act %>% group_by(date) %>% summarize(total = sum(steps, na.rm = F))
+g <- ggplot(sum, aes(total)) + geom_histogram(binwidth = 2000, boundary = 0, na.rm = T)  # It's important to try different binwidths
+g + labs(title = "Total Number of Steps Taken Each Day") + labs(x = "Total Number of Steps Taken Each Day", y = "Days")
+
+# g + geom_vline(xintercept = mean(sum$total, na.rm = T)) + geom_vline(xintercept = median(sum$total, na.rm = T))  # They're too close to show simultaneously
+# mean(sum$total, na.rm = T)
+# median(sum$total, na.rm = T)
+
+dev.copy(png, file = "./RepData_PeerAssessment1/figures/Q2.png")
+dev.off()
--- loadproc.R
+++ loadproc.R
@@ -1,4 +1,7 @@
 # Loading and preprocessing the data
+if (!file.exists("./RepData_PeerAssessment1/figures")) {
+  dir.create("./RepData_PeerAssessment1/figures")
+}
 unzip("./RepData_PeerAssessment1/activity.zip", exdir = "./RepData_PeerAssessment1/activity")
 act <- read.csv("./RepData_PeerAssessment1/activity/activity.csv")
 act$date <- as.Date(act$date, "%Y-%m-%d")
