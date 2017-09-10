61e7371ecd66f9bbb6a501bbec6e83ba69e0e24f
chswei <v09029w@gmail.com>
2017-09-10 18:50:53 +0800

    Revised Q6 & Finished Q7

--- Q7.R
+++ Q7.R
@@ -8,15 +8,18 @@ act <- read.csv("./RepData_PeerAssessment1/activity/activity.csv")
 act$date <- as.Date(act$date, "%Y-%m-%d")
 
 # 6. Code to describe and show a strategy for imputing missing data
-# 6.1 Calculate and report the total number of missing values in the dataset 
-# (i.e. the total number of rows with NAs)
-sum(is.na(act$steps))
-# 6.2 Devise a strategy for filling in all of the missing values in the dataset
 library(dplyr)
 library(ggplot2)
 avg <- act %>% group_by(interval) %>% summarize(avg = mean(steps, na.rm = T))
 act.na <- act[is.na(act$steps), ]
 act[is.na(act$steps), 1] <- avg[avg$interval %in% as.character(act.na[, 3]), 2]
-# 6.3 Create a new dataset that is equal to the original dataset but 
-# with the missing data filled in.
-write.csv(act, file = "./RepData_PeerAssessment1/imputedna.csv")
+
+# 7. Histogram of the total number of steps taken each day 
+# after missing values are imputed
+sum <- act %>% group_by(date) %>% summarize(total = sum(steps))
+g <- ggplot(sum, aes(total)) + 
+  geom_histogram(binwidth = 2000, boundary = 0, na.rm = T)  
+g + labs(title = "Total Number of Steps Taken Each Day") + 
+  labs(x = "Total Number of Steps Taken Each Day", y = "Days")
+dev.copy(png, file = "./RepData_PeerAssessment1/figures/Q7.png")
+dev.off()
